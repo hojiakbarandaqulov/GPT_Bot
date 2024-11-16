e384802a3e05b7e414f1756131b25cb8950b3ce1
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2024-11-16 12:37:46 +0500

    first commit

---
+++ src/main/java/com/example/GptBotApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptBotApplication.class, args);
+    }
+
+}
